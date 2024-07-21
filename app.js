f022b5df219ee0dfd09dbe09d8d4b9db70f54abb
macnemoto <marcosj465@gmail.com>
2024-07-21 19:13:07 -0400

    extract csv data

--- app.js
+++ app.js
@@ -1,34 +1,50 @@
-import webpconv from "@caed0/webp-conv";
-import sharp from "sharp";
-import fs from "fs";
+// import webpconv from "@caed0/webp-conv";
+// import sharp from "sharp";
+import csv from 'csv-parser';
+import fs from 'fs';
 
-const isWebAnimated = async (filePath) => {
-  try {
-    const buffer = fs.readFileSync(filePath);
-    const metadata = await sharp(buffer).metadata();
-    const isAnimated = metadata.pages > 1;
-    return isAnimated;
-  } catch (error) {
-    console.error("Error", error);
-  }
-};
 
-const getFileName = (input) => {
-  const parts = input.split(".");
-  return parts[0];
-};
+const links = [];
 
-const input = "peposalto2.webp";
+fs.createReadStream('stickers.csv')
+  .pipe(csv())
+  .on('data', (data) => {
+    const originalLink = data.link
+    const modifiedLin = originalLink.replace('https://7tv.app/emotes/','https://cdn.7tv.app/emote/') + '/4x.webp'
+    links.push(modifiedLin);
+    
+  })
+  .on('end', () => {
+    console.log('Valores de la columna "link":', links);
+  });
 
-const typeToConverter = await isWebAnimated(input);
-// console.log(typeToConverter)
-const name = getFileName(input);
-const output = `stickers-output/${name}${typeToConverter ? ".gif" : ".png"}`;
-console.log(output);
+// const isWebAnimated = async (filePath) => {
+//   try {
+//     const buffer = fs.readFileSync(filePath);
+//     const metadata = await sharp(buffer).metadata();
+//     const isAnimated = metadata.pages > 1;
+//     return isAnimated;
+//   } catch (error) {
+//     console.error("Error", error);
+//   }
+// };
 
-try {
-  const converter = new webpconv();
-  converter.convert(input, output, { quality: 10, transparent: "0x000000" });
-} catch (error) {
-  console.log(error);
-}
+// const getFileName = (input) => {
+//   const parts = input.split(".");
+//   return parts[0];
+// };
+
+// const input = "peposalto2.webp";
+
+// const typeToConverter = await isWebAnimated(input);
+// // console.log(typeToConverter)
+// const name = getFileName(input);
+// const output = `stickers-output/${name}${typeToConverter ? ".gif" : ".png"}`;
+// console.log(output);
+
+// try {
+//   const converter = new webpconv();
+//   converter.convert(input, output, { quality: 10, transparent: "0x000000" });
+// } catch (error) {
+//   console.log(error);
+// }
