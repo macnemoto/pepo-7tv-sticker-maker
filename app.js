4dbb98fcedb164142ac9777e20342f1f84b6d026
macnemoto <marcosj465@gmail.com>
2024-07-22 08:38:24 -0400

    download webp stickers in folder output

--- app.js
+++ app.js
@@ -1,22 +1,58 @@
 // import webpconv from "@caed0/webp-conv";
 // import sharp from "sharp";
-import csv from 'csv-parser';
-import fs from 'fs';
-
+import csv from "csv-parser";
+import fs from "fs";
+import axios from "axios";
+// import path from "path";
 
 const links = [];
+const allLinks = [];
 
-fs.createReadStream('stickers.csv')
+const dowloadImage = async (url) => {
+
+  try {
+const response = await axios.get(url, {
+  responseType: 'arraybuffer'
+})
+const imageName = url.split('/')
+console.log(typeof(imageName[4]))
+// const imageName = 'test22.webp'
+// console.log(imageName[4])
+
+fs.writeFileSync(`stickers-input/${imageName[4]}.webp`, response.data)
+console.log(`Imagen descargada: ${imageName}`);
+  } catch (error) {
+    console.error(`Error al descargar la imagen desde ${url}: ${error.message}`);
+  }
+};
+
+
+const downloadAllImages = async (links) => {
+  // console.log(links)
+  for (const link of links) {
+    await dowloadImage(link)
+  }
+}
+// -----------------------
+
+fs.createReadStream("stickers.csv")
   .pipe(csv())
-  .on('data', (data) => {
-    const originalLink = data.link
-    const modifiedLin = originalLink.replace('https://7tv.app/emotes/','https://cdn.7tv.app/emote/') + '/4x.webp'
-    links.push(modifiedLin);
-    
+  .on("data", (data) => {
+    const originalLink = data.link;
+    const modifiedLink =
+      originalLink.replace(
+        "https://7tv.app/emotes/",
+        "https://cdn.7tv.app/emote/"
+      ) + "/4x.webp";
+      allLinks.push(modifiedLink);
+    links.push(modifiedLink);
+
   })
-  .on('end', () => {
-    console.log('Valores de la columna "link":', links);
+  .on("end", () => {
+    // console.log('Valores de la columna "link":', links);
+    downloadAllImages(links)
   });
+
 
 // const isWebAnimated = async (filePath) => {
 //   try {
